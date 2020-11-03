568ae27f1f08a7e711853f0bb3fd1b9790f19062
Pelissero-Luca <62608324+Pelissero-Luca@users.noreply.github.com>
2020-11-03 16:15:48 +0100

    Updated

---
+++ Es 8 - clsSingleton+coda di stampa/008_Singleton+Queue/clsSingleton.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _008_Singleton_Queue
+{
+    class clsSingleton
+    {
+        private static clsSingleton _instace = null;
+        public static Queue<string> codaStampa = new Queue<string>();
+
+        private clsSingleton()
+        {
+
+        }
+
+        public void add (string s)
+        {
+            codaStampa.Enqueue(s);
+            System.Windows.Forms.MessageBox.Show(s + " aggiunto alla coda");
+        }
+
+        public void stampa ()
+        {
+            try
+            {
+                string s = codaStampa.Dequeue();
+                System.Windows.Forms.MessageBox.Show(s);
+            }
+            catch (Exception)
+            {
+                System.Windows.Forms.MessageBox.Show("Coda vuota");
+            }
+        }
+
+        public static clsSingleton GetClsSingleton()
+        {
+            if (_instace == null)
+            {
+                _instace = new clsSingleton();
+            }
+            return _instace;
+        }
+    }
+}
