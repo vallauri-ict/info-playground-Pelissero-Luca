9c7144cc944dc098b79defa266527dc117006e4b
Pelissero-Luca <62608324+Pelissero-Luca@users.noreply.github.com>
2020-11-11 11:09:23 +0100

    Updated

---
+++ Es 11 - RegEx Form di Registrazione/Es 11 - RegEx Form di Registrazione/Form1.Designer.cs
@@ -0,0 +1,150 @@
+﻿namespace Es_11___RegEx_Form_di_Registrazione
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtCognome = new System.Windows.Forms.TextBox();
+            this.txtNome = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.txtCap = new System.Windows.Forms.Label();
+            this.txtMail = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.btnRegistra = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 32);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(68, 17);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Cognome";
+            // 
+            // txtCognome
+            // 
+            this.txtCognome.Location = new System.Drawing.Point(132, 29);
+            this.txtCognome.Name = "txtCognome";
+            this.txtCognome.Size = new System.Drawing.Size(100, 22);
+            this.txtCognome.TabIndex = 1;
+            // 
+            // txtNome
+            // 
+            this.txtNome.Location = new System.Drawing.Point(132, 57);
+            this.txtNome.Name = "txtNome";
+            this.txtNome.Size = new System.Drawing.Size(100, 22);
+            this.txtNome.TabIndex = 3;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(34, 60);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(45, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Nome";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(132, 200);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 5;
+            // 
+            // txtCap
+            // 
+            this.txtCap.AutoSize = true;
+            this.txtCap.Location = new System.Drawing.Point(34, 203);
+            this.txtCap.Name = "txtCap";
+            this.txtCap.Size = new System.Drawing.Size(35, 17);
+            this.txtCap.TabIndex = 4;
+            this.txtCap.Text = "CAP";
+            // 
+            // txtMail
+            // 
+            this.txtMail.Location = new System.Drawing.Point(132, 228);
+            this.txtMail.Name = "txtMail";
+            this.txtMail.Size = new System.Drawing.Size(100, 22);
+            this.txtMail.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(34, 231);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(33, 17);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Mail";
+            // 
+            // btnRegistra
+            // 
+            this.btnRegistra.Location = new System.Drawing.Point(37, 340);
+            this.btnRegistra.Name = "btnRegistra";
+            this.btnRegistra.Size = new System.Drawing.Size(83, 30);
+            this.btnRegistra.TabIndex = 8;
+            this.btnRegistra.Text = "Registra";
+            this.btnRegistra.UseVisualStyleBackColor = true;
+            this.btnRegistra.Click += new System.EventHandler(this.btnRegistra_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegistra);
+            this.Controls.Add(this.txtMail);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.txtNome);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtCognome);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtCognome;
+        private System.Windows.Forms.TextBox txtNome;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label txtCap;
+        private System.Windows.Forms.TextBox txtMail;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button btnRegistra;
+    }
+}
+
---
+++ Es 11 - RegEx Form di Registrazione/Es 11 - RegEx Form di Registrazione/Form1.cs
@@ -0,0 +1,82 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Es_11___RegEx_Form_di_Registrazione
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegistra_Click(object sender, EventArgs e)
+        {
+            // che metto a false se una dele rgx non è rispettata
+            bool valido = true;
+
+            // COGNOME
+            Regex regCognome = new Regex(@"^[A-Z]{1}[a-z]+$");
+            if (txtCognome.Text == "")
+            {
+                MessageBox.Show("Cognome non inserito");
+                valido = false;
+                txtCognome.BackColor = Color.Salmon;
+            }
+            else if(!regCognome.IsMatch(txtCognome.Text))
+            {
+                MessageBox.Show("Cognome inserito non valido!");
+                valido = false;
+                txtCognome.BackColor = Color.Salmon;
+            }
+            else
+            {
+                // Inserire nella classe utente
+            }
+
+            // NOME
+            Regex regNome = new Regex(@"^[A-Z]{1}[a-z]+$");
+            if (txtNome.Text == "")
+            {
+                MessageBox.Show("Nome non inserito");
+                valido = false;
+                txtNome.BackColor = Color.Salmon;
+            }
+            else if (!regNome.IsMatch(txtNome.Text))
+            {
+                MessageBox.Show("Nome inserito non valido!");
+                valido = false;
+                txtNome.BackColor = Color.Salmon;
+            }
+            else
+            {
+                // Inserire nella classe utente
+            }
+
+            // GLI ALTRI CAMPI
+
+            // CAP (5 numeri)
+
+            /* MAIL 
+             inizia con: dei caratteri, '.', '_', '-'
+             poi '@'
+             poi alcuni caratteri
+             poi '.'
+             poi da 2 a 4 caratteri
+            */
+
+            /* PASSWORD
+             da 8 a 20 caratteri
+             ^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$
+            */
+        }
+    }
+}
