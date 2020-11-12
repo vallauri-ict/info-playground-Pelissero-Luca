dfa32007ea6dfdb00392e93ff30d6a47a72a0454
Pelissero-Luca <62608324+Pelissero-Luca@users.noreply.github.com>
2020-11-12 20:17:02 +0100

    Updated

--- Es 11 - RegEx Form di Registrazione/Es 11 - RegEx Form di Registrazione/Form1.Designer.cs
+++ Es 11 - RegEx Form di Registrazione/Es 11 - RegEx Form di Registrazione/Form1.Designer.cs
@@ -32,11 +32,21 @@
             this.txtCognome = new System.Windows.Forms.TextBox();
             this.txtNome = new System.Windows.Forms.TextBox();
             this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.txtCap = new System.Windows.Forms.Label();
+            this.txtCap = new System.Windows.Forms.TextBox();
+            this.lbl = new System.Windows.Forms.Label();
             this.txtMail = new System.Windows.Forms.TextBox();
             this.label3 = new System.Windows.Forms.Label();
             this.btnRegistra = new System.Windows.Forms.Button();
+            this.txtIndirizzo = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.txtCitta = new System.Windows.Forms.TextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.txtCodiceFisc = new System.Windows.Forms.TextBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.txtUser = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.label8 = new System.Windows.Forms.Label();
             this.SuspendLayout();
             // 
             // label1
@@ -50,14 +60,14 @@
             // 
             // txtCognome
             // 
-            this.txtCognome.Location = new System.Drawing.Point(132, 29);
+            this.txtCognome.Location = new System.Drawing.Point(150, 32);
             this.txtCognome.Name = "txtCognome";
             this.txtCognome.Size = new System.Drawing.Size(100, 22);
             this.txtCognome.TabIndex = 1;
             // 
             // txtNome
             // 
-            this.txtNome.Location = new System.Drawing.Point(132, 57);
+            this.txtNome.Location = new System.Drawing.Point(150, 60);
             this.txtNome.Name = "txtNome";
             this.txtNome.Size = new System.Drawing.Size(100, 22);
             this.txtNome.TabIndex = 3;
@@ -71,25 +81,25 @@
             this.label2.TabIndex = 2;
             this.label2.Text = "Nome";
             // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(132, 200);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(100, 22);
-            this.textBox1.TabIndex = 5;
-            // 
             // txtCap
             // 
-            this.txtCap.AutoSize = true;
-            this.txtCap.Location = new System.Drawing.Point(34, 203);
+            this.txtCap.Location = new System.Drawing.Point(150, 144);
             this.txtCap.Name = "txtCap";
-            this.txtCap.Size = new System.Drawing.Size(35, 17);
-            this.txtCap.TabIndex = 4;
-            this.txtCap.Text = "CAP";
+            this.txtCap.Size = new System.Drawing.Size(100, 22);
+            this.txtCap.TabIndex = 5;
+            // 
+            // lbl
+            // 
+            this.lbl.AutoSize = true;
+            this.lbl.Location = new System.Drawing.Point(34, 144);
+            this.lbl.Name = "lbl";
+            this.lbl.Size = new System.Drawing.Size(35, 17);
+            this.lbl.TabIndex = 4;
+            this.lbl.Text = "CAP";
             // 
             // txtMail
             // 
-            this.txtMail.Location = new System.Drawing.Point(132, 228);
+            this.txtMail.Location = new System.Drawing.Point(150, 172);
             this.txtMail.Name = "txtMail";
             this.txtMail.Size = new System.Drawing.Size(100, 22);
             this.txtMail.TabIndex = 7;
@@ -97,7 +107,7 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(34, 231);
+            this.label3.Location = new System.Drawing.Point(34, 172);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(33, 17);
             this.label3.TabIndex = 6;
@@ -105,24 +115,114 @@
             // 
             // btnRegistra
             // 
-            this.btnRegistra.Location = new System.Drawing.Point(37, 340);
+            this.btnRegistra.Location = new System.Drawing.Point(37, 293);
             this.btnRegistra.Name = "btnRegistra";
-            this.btnRegistra.Size = new System.Drawing.Size(83, 30);
+            this.btnRegistra.Size = new System.Drawing.Size(213, 30);
             this.btnRegistra.TabIndex = 8;
             this.btnRegistra.Text = "Registra";
             this.btnRegistra.UseVisualStyleBackColor = true;
             this.btnRegistra.Click += new System.EventHandler(this.btnRegistra_Click);
             // 
+            // txtIndirizzo
+            // 
+            this.txtIndirizzo.Location = new System.Drawing.Point(150, 88);
+            this.txtIndirizzo.Name = "txtIndirizzo";
+            this.txtIndirizzo.Size = new System.Drawing.Size(100, 22);
+            this.txtIndirizzo.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(34, 88);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(60, 17);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "Indirizzo";
+            // 
+            // txtCitta
+            // 
+            this.txtCitta.Location = new System.Drawing.Point(150, 116);
+            this.txtCitta.Name = "txtCitta";
+            this.txtCitta.Size = new System.Drawing.Size(100, 22);
+            this.txtCitta.TabIndex = 12;
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(34, 116);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(36, 17);
+            this.label5.TabIndex = 11;
+            this.label5.Text = "Città";
+            // 
+            // txtCodiceFisc
+            // 
+            this.txtCodiceFisc.Location = new System.Drawing.Point(150, 200);
+            this.txtCodiceFisc.Name = "txtCodiceFisc";
+            this.txtCodiceFisc.Size = new System.Drawing.Size(100, 22);
+            this.txtCodiceFisc.TabIndex = 14;
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(34, 200);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(99, 17);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Codice Fiscale";
+            // 
+            // txtUser
+            // 
+            this.txtUser.Location = new System.Drawing.Point(150, 228);
+            this.txtUser.Name = "txtUser";
+            this.txtUser.Size = new System.Drawing.Size(100, 22);
+            this.txtUser.TabIndex = 16;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(34, 228);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(38, 17);
+            this.label7.TabIndex = 15;
+            this.label7.Text = "User";
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(150, 256);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(100, 22);
+            this.txtPassword.TabIndex = 18;
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(34, 259);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(69, 17);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "Password";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(292, 340);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.txtUser);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.txtCodiceFisc);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.txtCitta);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.txtIndirizzo);
+            this.Controls.Add(this.label4);
             this.Controls.Add(this.btnRegistra);
             this.Controls.Add(this.txtMail);
             this.Controls.Add(this.label3);
-            this.Controls.Add(this.textBox1);
             this.Controls.Add(this.txtCap);
+            this.Controls.Add(this.lbl);
             this.Controls.Add(this.txtNome);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.txtCognome);
@@ -140,11 +240,21 @@
         private System.Windows.Forms.TextBox txtCognome;
         private System.Windows.Forms.TextBox txtNome;
         private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label txtCap;
+        private System.Windows.Forms.TextBox txtCap;
+        private System.Windows.Forms.Label lbl;
         private System.Windows.Forms.TextBox txtMail;
         private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Button btnRegistra;
+        private System.Windows.Forms.TextBox txtIndirizzo;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox txtCitta;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox txtCodiceFisc;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox txtUser;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.Label label8;
     }
 }
 
--- Es 11 - RegEx Form di Registrazione/Es 11 - RegEx Form di Registrazione/Form1.cs
+++ Es 11 - RegEx Form di Registrazione/Es 11 - RegEx Form di Registrazione/Form1.cs
@@ -3,7 +3,9 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
+using System.Security.Cryptography.X509Certificates;
 using System.Text;
 using System.Text.RegularExpressions;
 using System.Threading.Tasks;
@@ -22,61 +24,200 @@ namespace Es_11___RegEx_Form_di_Registrazione
         {
             // che metto a false se una dele rgx non è rispettata
             bool valido = true;
+            string info = "";
 
-            // COGNOME
-            Regex regCognome = new Regex(@"^[A-Z]{1}[a-z]+$");
-            if (txtCognome.Text == "")
+            // USER
+            Regex regUser = new Regex(@"^.{1,20}$");
+            if (txtUser.Text == "")
             {
-                MessageBox.Show("Cognome non inserito");
+                MessageBox.Show("User Fiscale non inserito");
                 valido = false;
-                txtCognome.BackColor = Color.Salmon;
+                txtUser.BackColor = Color.Salmon;
             }
-            else if(!regCognome.IsMatch(txtCognome.Text))
+            else if (!regUser.IsMatch(txtUser.Text))
             {
-                MessageBox.Show("Cognome inserito non valido!");
+                MessageBox.Show("User inserito non valido!");
                 valido = false;
-                txtCognome.BackColor = Color.Salmon;
+                txtUser.BackColor = Color.Salmon;
             }
             else
             {
-                // Inserire nella classe utente
+                clsUtenti user = new clsUtenti();
+
+                if (user.controllaUser(txtUser.Text) == true)
+                {
+                    user.user(txtUser.Text);
+                    // COGNOME
+                    Regex regCognome = new Regex(@"^[A-Z]{1}[a-z]+$");
+                    if (txtCognome.Text == "")
+                    {
+                        MessageBox.Show("Cognome non inserito");
+                        valido = false;
+                        txtCognome.BackColor = Color.Salmon;
+                    }
+                    else if (!regCognome.IsMatch(txtCognome.Text))
+                    {
+                        MessageBox.Show("Cognome inserito non valido!");
+                        valido = false;
+                        txtCognome.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info = txtCognome.Text + "\t";
+                    }
+
+                    // NOME
+                    Regex regNome = new Regex(@"^[A-Z]{1}[a-z]+$");
+                    if (txtNome.Text == "")
+                    {
+                        MessageBox.Show("Nome non inserito");
+                        valido = false;
+                        txtNome.BackColor = Color.Salmon;
+                    }
+                    else if (!regNome.IsMatch(txtNome.Text))
+                    {
+                        MessageBox.Show("Nome inserito non valido!");
+                        valido = false;
+                        txtNome.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtNome.Text + "\t";
+                    }
+
+                    // INDIRIZZO
+                    Regex regIndirizzo = new Regex(@"^[A-Z][a-z]{2}\ [A-Z][a-z]*\ \d{1,2}$");
+                    if (txtIndirizzo.Text == "")
+                    {
+                        MessageBox.Show("Indirizzo non inserito");
+                        valido = false;
+                        txtIndirizzo.BackColor = Color.Salmon;
+                    }
+                    else if (!regIndirizzo.IsMatch(txtIndirizzo.Text))
+                    {
+                        MessageBox.Show("Indirizzo inserito non valido!");
+                        valido = false;
+                        txtIndirizzo.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtIndirizzo.Text + "\t";
+                    }
+
+                    // CITTA'
+                    Regex regCitta = new Regex(@"^[A-Z]{1}[a-z]+$");
+                    if (txtCitta.Text == "")
+                    {
+                        MessageBox.Show("Città non inserita");
+                        valido = false;
+                        txtCitta.BackColor = Color.Salmon;
+                    }
+                    else if (!regCitta.IsMatch(txtCitta.Text))
+                    {
+                        MessageBox.Show("Città inserita non valida!");
+                        valido = false;
+                        txtCitta.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtCitta.Text + "\t";
+                    }
+
+                    // CAP 
+                    Regex regCap = new Regex(@"^\d{5}$");
+                    if (txtCap.Text == "")
+                    {
+                        MessageBox.Show("CAP non inserito");
+                        valido = false;
+                        txtCap.BackColor = Color.Salmon;
+                    }
+                    else if (!regCap.IsMatch(txtCap.Text))
+                    {
+                        MessageBox.Show("CAP inserito non valido!");
+                        valido = false;
+                        txtCap.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtCap.Text + "\t";
+                    }
+
+                    // MAIL
+                    Regex regMail = new Regex(@"^.*(?<!\s)\@[a-zA-Z]{2,8}(?!\s)\.[a-zA-Z]{2,4}(?!\s)$");
+                    if (txtMail.Text == "")
+                    {
+                        MessageBox.Show("Mail non inserita");
+                        valido = false;
+                        txtMail.BackColor = Color.Salmon;
+                    }
+                    else if (!regMail.IsMatch(txtMail.Text))
+                    {
+                        MessageBox.Show("Mail inserita non valida!");
+                        valido = false;
+                        txtMail.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtMail.Text + "\t";
+                    }
+
+                    // CODICE FISCALE
+                    Regex regCodiceFiscale = new Regex(@"^[A-Z]{6}\d{2}[A-Z]\d{2}[A-Z]\d{3}[A-Z]$");
+                    if (txtCodiceFisc.Text == "")
+                    {
+                        MessageBox.Show("Codice Fiscale non inserito");
+                        valido = false;
+                        txtCodiceFisc.BackColor = Color.Salmon;
+                    }
+                    else if (!regCodiceFiscale.IsMatch(txtCodiceFisc.Text))
+                    {
+                        MessageBox.Show("Codice Fiscale inserito non valido!");
+                        valido = false;
+                        txtCodiceFisc.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtCodiceFisc.Text + "\t";
+                    }
+
+                    // PASSWORD
+                    Regex regPassword = new Regex(@"^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$");
+                    if (txtPassword.Text == "")
+                    {
+                        MessageBox.Show("Password non inserita");
+                        valido = false;
+                        txtPassword.BackColor = Color.Salmon;
+                    }
+                    else if (!regPassword.IsMatch(txtPassword.Text))
+                    {
+                        MessageBox.Show("Password inserita non valida!");
+                        valido = false;
+                        txtPassword.BackColor = Color.Salmon;
+                    }
+                    else
+                    {
+                        // Inserire nella classe utente
+                        info += txtPassword.Text + "\t";
+                    }
+                }
+                else
+                    MessageBox.Show("User già esistente");
+
+                if (valido)
+                {
+                    StreamWriter sw = new StreamWriter("Utenti.txt", true);
+                    sw.WriteLine(info);
+                    sw.Close();
+                    MessageBox.Show("Dati registrati correttamente");
+                }
             }
-
-            // NOME
-            Regex regNome = new Regex(@"^[A-Z]{1}[a-z]+$");
-            if (txtNome.Text == "")
-            {
-                MessageBox.Show("Nome non inserito");
-                valido = false;
-                txtNome.BackColor = Color.Salmon;
-            }
-            else if (!regNome.IsMatch(txtNome.Text))
-            {
-                MessageBox.Show("Nome inserito non valido!");
-                valido = false;
-                txtNome.BackColor = Color.Salmon;
-            }
-            else
-            {
-                // Inserire nella classe utente
-            }
-
-            // GLI ALTRI CAMPI
-
-            // CAP (5 numeri)
-
-            /* MAIL 
-             inizia con: dei caratteri, '.', '_', '-'
-             poi '@'
-             poi alcuni caratteri
-             poi '.'
-             poi da 2 a 4 caratteri
-            */
-
-            /* PASSWORD
-             da 8 a 20 caratteri
-             ^(?=.*\d)(?=.*[a-z])(?=.*[A-Z]).{8,20}$
-            */
         }
     }
 }
