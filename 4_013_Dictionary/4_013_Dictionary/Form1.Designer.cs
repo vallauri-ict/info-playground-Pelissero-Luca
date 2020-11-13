d57f4ffd4693cf3923b8ce0f87c337e746006c92
Pelissero-Luca <62608324+Pelissero-Luca@users.noreply.github.com>
2020-11-13 16:00:39 +0100

    Gestione Dictionary

---
+++ 4_013_Dictionary/4_013_Dictionary/Form1.Designer.cs
@@ -0,0 +1,119 @@
+﻿namespace _4_013_Dictionary
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variabile di progettazione necessaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Pulire le risorse in uso.
+        /// </summary>
+        /// <param name="disposing">ha valore true se le risorse gestite devono essere eliminate, false in caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Codice generato da Progettazione Windows Form
+
+        /// <summary>
+        /// Metodo necessario per il supporto della finestra di progettazione. Non modificare
+        /// il contenuto del metodo con l'editor di codice.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnVisualizza = new System.Windows.Forms.Button();
+            this.bntAdd = new System.Windows.Forms.Button();
+            this.txtAutore = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTitolo = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btnVisualizza
+            // 
+            this.btnVisualizza.Location = new System.Drawing.Point(41, 147);
+            this.btnVisualizza.Name = "btnVisualizza";
+            this.btnVisualizza.Size = new System.Drawing.Size(189, 32);
+            this.btnVisualizza.TabIndex = 15;
+            this.btnVisualizza.Text = "Visualizza";
+            this.btnVisualizza.UseVisualStyleBackColor = true;
+            this.btnVisualizza.Click += new System.EventHandler(this.btnVisualizza_Click);
+            // 
+            // bntAdd
+            // 
+            this.bntAdd.Location = new System.Drawing.Point(41, 109);
+            this.bntAdd.Name = "bntAdd";
+            this.bntAdd.Size = new System.Drawing.Size(189, 32);
+            this.bntAdd.TabIndex = 14;
+            this.bntAdd.Text = "Aggiungi";
+            this.bntAdd.UseVisualStyleBackColor = true;
+            this.bntAdd.Click += new System.EventHandler(this.bntAdd_Click);
+            // 
+            // txtAutore
+            // 
+            this.txtAutore.Location = new System.Drawing.Point(130, 65);
+            this.txtAutore.Name = "txtAutore";
+            this.txtAutore.Size = new System.Drawing.Size(100, 22);
+            this.txtAutore.TabIndex = 11;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(38, 68);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(50, 17);
+            this.label2.TabIndex = 10;
+            this.label2.Text = "Autore";
+            // 
+            // txtTitolo
+            // 
+            this.txtTitolo.Location = new System.Drawing.Point(130, 37);
+            this.txtTitolo.Name = "txtTitolo";
+            this.txtTitolo.Size = new System.Drawing.Size(100, 22);
+            this.txtTitolo.TabIndex = 9;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(38, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(43, 17);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Titolo";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(270, 194);
+            this.Controls.Add(this.btnVisualizza);
+            this.Controls.Add(this.bntAdd);
+            this.Controls.Add(this.txtAutore);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtTitolo);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnVisualizza;
+        private System.Windows.Forms.Button bntAdd;
+        private System.Windows.Forms.TextBox txtAutore;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTitolo;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ 4_013_Dictionary/4_013_Dictionary/Form1.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _4_013_Dictionary
+{
+    public partial class Form1 : Form
+    {
+        public struct Libro
+        {
+            public string Titolo;
+            public string Autore;
+        }
+        public int i = 0;
+        Dictionary<int, Libro> libreria = new Dictionary<int, Libro>();
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void bntAdd_Click(object sender, EventArgs e)
+        {
+            Libro l;
+            l.Titolo = txtTitolo.Text;
+            l.Autore = txtAutore.Text;
+            libreria.Add(i, l);
+            i++;
+        }
+
+        private void btnVisualizza_Click(object sender, EventArgs e)
+        {
+            foreach (var key in libreria.Keys)
+                MessageBox.Show("Chiave: " + Convert.ToString(key));
+
+            foreach (var item in libreria.Values)
+                MessageBox.Show("Autore: " + item.Autore + "\nTitolo: " + item.Titolo);
+        }
+    }
+}
